601c1c8a916e34124373dc5fd9c2f4451a864bfd
naveen g l <glnaveen06@gmail.com>
2022-10-30 05:15:01 +0000

    added new files

---
+++ src/Application.java
@@ -0,0 +1,12 @@
+class Application{
+public static void main(String...args){
+
+}
+
+public static void applicationDevolop(){
+
+System.out.println("Code to devolop an application");
+}
+
+
+}
---
+++ test/ApplicationTest.java
@@ -0,0 +1,9 @@
+class ApplicationTest{
+
+public static void applicationTest(){
+
+System.out.println("Application Test");
+}
+
+
+}
