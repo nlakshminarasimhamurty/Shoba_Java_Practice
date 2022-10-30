2adcef0367248cc772b238c705a017426c014ee4
naveen g l <glnaveen06@gmail.com>
2022-10-30 06:04:21 +0000

    modified file

--- src/Application.java
+++ src/Application.java
@@ -6,6 +6,7 @@ public static void main(String...args){
 public static void applicationDevolop(){
 
 System.out.println("Code to devolop an application");
+System.out.println("added one more line");
 }
 
 
